0bc3347c731145a700dab1fbc8ddbbefd7ee25a0
CESAR NERI SANCHEZ <cesarsanchez678@outlook.es>
2024-06-20 17:26:21 -0600

    Funcionalidad de botones para redirigir a las vistas

--- Controllers/SubModuleController.cs
+++ Controllers/SubModuleController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using WebAppTruck.Models.Services;
 using WebAppTruck.Models.ViewModels;
-using Microsoft.AspNetCore.Authorization;
 
 namespace WebAppTruck.Controllers
 {
